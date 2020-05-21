43c4760335ea7039886e79e27ec72ecafbd81dd7
harrisonglotz <harrisonglotz@gmail.com>
2020-05-21 11:26:52 -0400

    added timezone setup

--- setup.py
+++ setup.py
@@ -5,21 +5,26 @@ import time
 #set up debian server on proxmox
 
 #update server
-print('\n*****Getting updates...*****\n\n')
+print('\n=====Getting updates=====\n\n')
 os.system('apt-get update')
 os.system('apt-get upgrade')
 
 #install preferred packages
-print('\n*****Getting preferred packages...*****\n\n')
+print('\n=====Getting preferred packages=====\n\n')
 os.system('apt-get install sudo vim nano wget net-tools')
 
 #start SSH
-print('\n*****Starting SSH...*****\n\n')
+print('\n=====Starting SSH=====\n\n')
 os.system('systemctl start sshd')
 os.system('systemctl restart sshd')
 
+#change timezone to Standard Est. Time
+print('\n=====Setting Timezone=====\n\n')
+os.system('timedatectl set-timezone America/New_York')
+os.system('timedatectl status')
+
 #create new sudo user
-print('\n*****Creating sudo user...*****\n\n')
+print('\n=====Creating sudo user=====\n\n')
 username = input('Enter username for sudo user: ')
 os.system(f'adduser {username}')
 os.system(f'passwd {username}')
